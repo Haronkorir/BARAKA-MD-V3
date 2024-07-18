e542d409426bcab4280101aaad74747048ecd426
ibraah adams <ibraah319@gmail.com>
2024-07-18 20:35:03 +0300

    Update conversion.js

--- bmw/conversion.js
+++ bmw/conversion.js
@@ -70,7 +70,7 @@ const alea = (ext) => {
     }
 
     sticker = new Sticker(buffer, {
-      pack:"Beltah-Md" ,
+      pack:"Baraka-Md" ,
       author: nomAuteurMessage,
       type:
         arg.includes("crop") || arg.includes("c")
